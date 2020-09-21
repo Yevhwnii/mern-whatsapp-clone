303dc7209b5374b7ed4700fb1df669583ddf2848
Yevhwnii <mirandoos3223@gmail.com>
2020-09-21 17:32:59 +0300

    intial sidebar markup

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 import './Sidebar.css';
 
+import { DonutLarge, Chat, MoreVert } from '@material-ui/icons';
+import { Avatar, IconButton } from '@material-ui/core';
+
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <h1>Sidebar</h1>
+      <div className='sidebar__header'>
+        <Avatar src='https://www.w3schools.com/howto/img_avatar.png' />
+        <div className='sidebar__headerRight'>
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
