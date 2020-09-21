bd029da8acc12dc2adcd7a9dfb9826e55b3f2cd4
Yevhwnii <mirandoos3223@gmail.com>
2020-09-21 17:06:38 +0300

    firebase config

---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBUr0wU40QIAH3Wk_lRdXLvBP3lhz5Ox24',
+  authDomain: 'whatsapp-mern-clone-2510f.firebaseapp.com',
+  databaseURL: 'https://whatsapp-mern-clone-2510f.firebaseio.com',
+  projectId: 'whatsapp-mern-clone-2510f',
+  storageBucket: 'whatsapp-mern-clone-2510f.appspot.com',
+  messagingSenderId: '491541797774',
+  appId: '1:491541797774:web:d489319bf4e7148ed962df',
+};
