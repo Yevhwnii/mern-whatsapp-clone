190768e59df0e7471f03849bd63763ad233f4c33
Yevhwnii <mirandoos3223@gmail.com>
2020-09-30 16:24:46 +0200

    subscribe to pusher from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chat from './containers/Chat';
 import Sidebar from './containers/Sidebar';
+import Pusher from 'pusher-js';
+
+import axios from './common/axios';
 
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/v1/messages/sync').then((res) => {
+      setMessages(res.data.messages);
+    });
+  }, []);
+  useEffect(() => {
+    const pusher = new Pusher('1127e3079bb622dfb4a1', {
+      cluster: 'eu',
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
   return (
     <div className='app'>
       <div className='app__body'>
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
